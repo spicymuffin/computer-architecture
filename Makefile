7078cc3c76e7f3e0984f79fc0357d99ab8cae2c0
spicymuffin <luigicussigh59@gmail.com>
2025-03-26 01:43:50 +0900

    return to previous state lol

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 main:
-	g++ -S -o benchmark_memory.s benchmark_memory.cpp
+	g++ -S -o benchmark_memory.s -g benchmark_memory.cpp
 clean:
 	rm -f benchmark_memory.s
 	rm -f benchmark
-benchmark:
-	g++ -o benchmark benchmark_memory.cpp -lbenchmark -lpthread -O3
+benchmark: benchmark_memory.cpp
+	g++ -o benchmark benchmark_memory.cpp -lbenchmark -lpthread
+run:
 	./benchmark
--- benchmark_memory.cpp
+++ benchmark_memory.cpp
@@ -19,39 +19,15 @@ static void BM_MemoryAccess(benchmark::State& state)
 }
 BENCHMARK(BM_MemoryAccess);
 
-#define UNROLL_DEPTH 3
-
 static void BM_OptimizedMemoryAccess(benchmark::State& state)
 {
     for (auto _ : state)
     {
-        int acc[UNROLL_DEPTH];
-
-        for (int i = 0; i < UNROLL_DEPTH; i++)
-        {
-            acc[i] = 0;
-        }
-        // sum unrolled
-        for (int i = 0; i < ARRAY_SIZE; i += UNROLL_DEPTH)
-        {
-            for (int j = 0; j < UNROLL_DEPTH; j++)
-            {
-                acc[j] += A[i + j];
-            }
-        }
-
-        // go through the remaining elements
-        for (int i = ARRAY_SIZE - ARRAY_SIZE % UNROLL_DEPTH; i < ARRAY_SIZE; i++)
-        {
-            acc[0] += A[i];
-        }
-
         int sum = 0;
-        for (int i = 0; i < UNROLL_DEPTH; i++)
+        for (int i = 0; i < ARRAY_SIZE; i++)
         {
-            sum += acc[i];
+            // Fill here!!
         }
-
         benchmark::DoNotOptimize(sum);
     }
 }
